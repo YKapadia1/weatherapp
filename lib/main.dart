89a63006dc1469b47cb9ec83e1741d44d38c6a52
Yamin Kapadia <YKapadia@uclan.ac.uk>
2022-03-07 20:03:46 +0000

    Fixed navigator bug

--- lib/main.dart
+++ lib/main.dart
@@ -10,60 +10,66 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: DefaultTabController(
-            length: 2,
-            child: Scaffold(
-              appBar: AppBar(
-                  bottom: const TabBar(
-                    tabs: [
-                      Tab(
-                        text: 'All',
-                      ),
-                      Tab(
-                        text: 'Favourites',
-                      ),
-                    ],
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+              bottom: const TabBar(
+                tabs: [
+                  Tab(
+                    text: 'All',
                   ),
-                  title: const Text('Home')),
-              body: TabBarView(
-                children: [
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'No cities added',
-                          style: Theme.of(context).textTheme.headline4,
-                        ),
-                        Text(
-                            'Tap the button in the bottom right to add a city and get started.',
-                            style: Theme.of(context).textTheme.headline5),
-                      ],
-                    ),
-                  ),
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('No favourite cities',
-                            style: Theme.of(context).textTheme.headline4),
-                      ],
-                    ),
+                  Tab(
+                    text: 'Favourites',
                   ),
                 ],
               ),
-              floatingActionButton: FloatingActionButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const AddCityRoute()));
-                },
-                tooltip: 'Increment',
-                child: const Icon(Icons.add_location),
+              title: const Text('Home')),
+          body: TabBarView(
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'No cities added',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    Text(
+                        'Tap the button in the bottom right to add a city and get started.',
+                        style: Theme.of(context).textTheme.headline5),
+                  ],
+                ),
               ),
-            )));
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('No favourite cities',
+                        style: Theme.of(context).textTheme.headline4),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AddCityRoute()));
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add_location),
+          ),
+        ));
   }
 }
 
