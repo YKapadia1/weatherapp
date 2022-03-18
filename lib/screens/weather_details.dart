7680c0b01712085474670c4701e3628c004841e2
YKapadia1 <YKapadia@uclan.ac.uk>
2022-03-18 11:14:35 +0000

    Added favourites icon and code

--- lib/screens/weather_details.dart
+++ lib/screens/weather_details.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/dependencies/weather_icons.dart';
+import 'package:weatherapp/dependencies/my_theme.dart';
+import 'package:weatherapp/dependencies/my_theme.dart';
+
+bool isFavourited = false;
 
 class WeatherDetailsRoute extends StatelessWidget {
-  const WeatherDetailsRoute({key: Key}) : super(key: key);
+  const WeatherDetailsRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,12 +15,27 @@ class WeatherDetailsRoute extends StatelessWidget {
         Padding(
           padding: EdgeInsets.only(right: 20.0),
           child: GestureDetector(
+              onTap: () {
+                if (isFavourited) {
+                  isFavourited = false;
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      AppTheme.DefaultSnackBar(
+                          "City removed from favourites."));
+                } else {
+                  isFavourited = true;
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      AppTheme.DefaultSnackBar("City added to favourites."));
+                }
+
+                (context as Element).reassemble();
+              },
               child: Icon(
-            Icons.favorite,
-            size: 26.0,
-          )),
+                isFavourited ? Icons.favorite : Icons.favorite_outline,
+                size: 26.0,
+              )),
         )
       ]),
+      body: Center(child: testIcon),
     );
   }
 }
