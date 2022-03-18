e6a2cd4f600b3dd4cc1a3bd3c5cedb2aee49a8bb
YKapadia1 <YKapadia@uclan.ac.uk>
2022-03-18 19:40:51 +0000

    Added more elements to be in line with design

--- lib/screens/weather_details.dart
+++ lib/screens/weather_details.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weatherapp/dependencies/weather_icons.dart';
 import 'package:weatherapp/dependencies/my_theme.dart';
-import 'package:weatherapp/dependencies/my_theme.dart';
 
 bool isFavourited = false;
 
@@ -11,31 +10,62 @@ class WeatherDetailsRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(right: 20.0),
-          child: GestureDetector(
-              onTap: () {
-                if (isFavourited) {
-                  isFavourited = false;
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      AppTheme.DefaultSnackBar(
-                          "City removed from favourites."));
-                } else {
-                  isFavourited = true;
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      AppTheme.DefaultSnackBar("City added to favourites."));
-                }
+        appBar: AppBar(actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+                onTap: () {
+                  if (isFavourited) {
+                    isFavourited = false;
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        AppTheme.DefaultSnackBar(
+                            "City removed from favourites."));
+                  } else {
+                    isFavourited = true;
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        AppTheme.DefaultSnackBar("City added to favourites."));
+                  }
 
-                (context as Element).reassemble();
-              },
-              child: Icon(
-                isFavourited ? Icons.favorite : Icons.favorite_outline,
-                size: 26.0,
-              )),
-        )
-      ]),
-      body: Center(child: testIcon),
-    );
+                  (context as Element).reassemble();
+                },
+                child: Icon(
+                  isFavourited ? Icons.favorite : Icons.favorite_outline,
+                  size: 26.0,
+                )),
+          )
+        ]),
+        body: Container(
+          alignment: Alignment.topCenter,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: clear_sky, fit: BoxFit.cover)),
+          child: Column(
+            children: <Widget>[
+              Text(
+                "City Name goes here",
+                style: TextStyle(color: Colors.white, fontSize: 34),
+              ),
+              Text(
+                "State, Country",
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+              icon_01d,
+              RichText(
+                  text: TextSpan(
+                      style: TextStyle(color: Colors.white, fontSize: 48),
+                      children: [
+                    TextSpan(text: "XX"),
+                    WidgetSpan(
+                        child: Transform.translate(
+                            offset: const Offset(0.0, -28.0),
+                            child: Text("o",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 24)))),
+                    TextSpan(
+                        text: "C",
+                        style: TextStyle(color: Colors.white, fontSize: 48))
+                  ]))
+            ],
+          ),
+        ));
   }
 }
