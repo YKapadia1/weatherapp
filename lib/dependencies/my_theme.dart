4bd7471c5074cdc7d217efb30f3e4e7993963653
YKapadia1 <YKapadia@uclan.ac.uk>
2022-03-18 10:26:26 +0000

    Added default snackbar to theme file

--- lib/dependencies/my_theme.dart
+++ lib/dependencies/my_theme.dart
@@ -1,27 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class AppTheme
-{
-  static TextTheme lightTextTheme = TextTheme
-  (
-    bodyText1: GoogleFonts.openSans(fontWeight: FontWeight.normal, fontSize: 16, color: Colors.black,wordSpacing: 0.5),
-    headline4: GoogleFonts.openSans(fontWeight: FontWeight.normal, fontSize:34, color: Colors.grey, wordSpacing: 0.25 ),
-    headline5: GoogleFonts.openSans(fontWeight: FontWeight.normal, fontSize:24, color: Colors.black, wordSpacing: 0 ),
-
-
+class AppTheme {
+  static TextTheme lightTextTheme = TextTheme(
+    bodyText1: GoogleFonts.openSans(
+        fontWeight: FontWeight.normal,
+        fontSize: 16,
+        color: Colors.black,
+        wordSpacing: 0.5),
+    headline4: GoogleFonts.openSans(
+        fontWeight: FontWeight.normal,
+        fontSize: 34,
+        color: Colors.grey,
+        wordSpacing: 0.25),
+    headline5: GoogleFonts.openSans(
+        fontWeight: FontWeight.normal,
+        fontSize: 24,
+        color: Colors.black,
+        wordSpacing: 0),
   );
-  static TextTheme darkTextTheme = TextTheme
-  (
-    bodyText1: GoogleFonts.openSans(fontWeight: FontWeight.normal, fontSize: 16, color: Colors.white,wordSpacing: 0.5),
-    headline4: GoogleFonts.openSans(fontWeight: FontWeight.normal, fontSize:34, color: Colors.grey, wordSpacing: 0.25 ),
-    headline5: GoogleFonts.openSans(fontWeight: FontWeight.normal, fontSize:24, color: Colors.white, wordSpacing: 0 ),
+  static TextTheme darkTextTheme = TextTheme(
+    bodyText1: GoogleFonts.openSans(
+        fontWeight: FontWeight.normal,
+        fontSize: 16,
+        color: Colors.white,
+        wordSpacing: 0.5),
+    headline4: GoogleFonts.openSans(
+        fontWeight: FontWeight.normal,
+        fontSize: 34,
+        color: Colors.grey,
+        wordSpacing: 0.25),
+    headline5: GoogleFonts.openSans(
+        fontWeight: FontWeight.normal,
+        fontSize: 24,
+        color: Colors.white,
+        wordSpacing: 0),
   );
 
-  static ThemeData light() 
-  {
-    return ThemeData
-    (brightness: Brightness.light,
+  static ThemeData light() {
+    return ThemeData(
+      brightness: Brightness.light,
       appBarTheme: const AppBarTheme(
         foregroundColor: Colors.white,
         backgroundColor: Colors.blue,
@@ -36,10 +54,9 @@ class AppTheme
       textTheme: lightTextTheme,
     );
   }
-  static ThemeData dark()
-  {
-    return ThemeData
-    (
+
+  static ThemeData dark() {
+    return ThemeData(
       brightness: Brightness.dark,
       appBarTheme: const AppBarTheme(
         foregroundColor: Colors.white,
@@ -55,21 +72,22 @@ class AppTheme
       textTheme: darkTextTheme,
     );
   }
-  static TextStyle lightText()
-  {
-    return TextStyle
-    (
+
+  static TextStyle lightText() {
+    return TextStyle(
       color: Colors.black,
-      fontSize: 18,
     );
   }
 
-  static TextStyle darkText()
-  {
-    return TextStyle
-    (
+  static TextStyle darkText() {
+    return TextStyle(
       color: Colors.white,
-      fontSize:18,
     );
+  }
+
+  static SnackBar DefaultSnackBar(String snackBarText) {
+    return SnackBar(
+        content: Text(snackBarText, style: darkText()),
+        backgroundColor: Colors.black);
   }
 }
