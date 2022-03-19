4a1fd235ac99cb386925e5d3cc5aa20210380edf
YKapadia1 <YKapadia@uclan.ac.uk>
2022-03-19 11:22:02 +0000

    Fully completed weather details screen

--- lib/screens/weather_details.dart
+++ lib/screens/weather_details.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:weatherapp/dependencies/weather_icons.dart';
+import 'package:weatherapp/dependencies/send_request.dart';
 import 'package:weatherapp/dependencies/my_theme.dart';
+import 'package:weatherapp/dependencies/json_parser.dart';
+import 'package:weatherapp/dependencies/db_handler.dart';
+import 'package:weatherapp/entry_table_model.dart';
 
-bool isFavourited = false;
+const apiKey = '769abfc9-64d7-4050-8cd3-79aecfda4830';
+
+const ICON_DIR = "image_assets/";
+const BACKGROUND_DIR = "image_assets/background_";
 
 class WeatherDetailsRoute extends StatelessWidget {
-  const WeatherDetailsRoute({Key? key}) : super(key: key);
+  WeatherDetailsRoute(
+      this.cityName, this.stateName, this.countryName, this.isFavourite);
+
+  late String? cityName;
+  late String? stateName;
+  late String? countryName;
+  late int isFavourite;
+
+  Future<Map> getWeather() async {
+    final jsonMap = await fetchWeatherData(cityName, stateName, countryName);
+    Map weatherMap = jsonMap['current']['weather'];
+    return weatherMap;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,57 +33,81 @@ class WeatherDetailsRoute extends StatelessWidget {
             padding: EdgeInsets.only(right: 20.0),
             child: GestureDetector(
                 onTap: () {
-                  if (isFavourited) {
-                    isFavourited = false;
+                  if (this.isFavourite == 1) {
+                    isFavourite = 0;
                     ScaffoldMessenger.of(context).showSnackBar(
                         AppTheme.DefaultSnackBar(
                             "City removed from favourites."));
                   } else {
-                    isFavourited = true;
+                    isFavourite = 1;
                     ScaffoldMessenger.of(context).showSnackBar(
                         AppTheme.DefaultSnackBar("City added to favourites."));
                   }
-
                   (context as Element).reassemble();
                 },
                 child: Icon(
-                  isFavourited ? Icons.favorite : Icons.favorite_outline,
+                  (isFavourite == 1) ? Icons.favorite : Icons.favorite_outline,
                   size: 26.0,
                 )),
           )
         ]),
-        body: Container(
-          alignment: Alignment.topCenter,
-          decoration: BoxDecoration(
-              image: DecorationImage(image: clear_sky, fit: BoxFit.cover)),
-          child: Column(
-            children: <Widget>[
-              Text(
-                "City Name goes here",
-                style: TextStyle(color: Colors.white, fontSize: 34),
-              ),
-              Text(
-                "State, Country",
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-              icon_01d,
-              RichText(
-                  text: TextSpan(
-                      style: TextStyle(color: Colors.white, fontSize: 48),
-                      children: [
-                    TextSpan(text: "XX"),
-                    WidgetSpan(
-                        child: Transform.translate(
+        body: FutureBuilder(
+          future: getWeather(),
+          builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
+            if (snapshot.hasData) {
+              var iconPath =
+                  ICON_DIR + snapshot.data!['ic'].toString() + ".png";
+              var weather_icon = Image.asset(
+                iconPath,
+                width: 100,
+                height: 100,
+              );
+              var backgroundPath =
+                  BACKGROUND_DIR + snapshot.data!['ic'].toString() + ".jpg";
+              var weather_background = AssetImage(backgroundPath);
+
+              return Container(
+                alignment: Alignment.topCenter,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: weather_background, fit: BoxFit.cover)),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      this.cityName.toString(),
+                      style: AppTheme.WeatherDetailsTextStyle(34),
+                    ),
+                    Text(
+                      this.stateName.toString() +
+                          ", " +
+                          this.countryName.toString(),
+                      style: AppTheme.WeatherDetailsTextStyle(24),
+                    ),
+                    weather_icon,
+                    RichText(
+                        text: TextSpan(
+                            style: AppTheme.WeatherDetailsTextStyle(48),
+                            children: [
+                          TextSpan(text: snapshot.data!['tp'].toString()),
+                          WidgetSpan(
+                              child: Transform.translate(
                             offset: const Offset(0.0, -28.0),
                             child: Text("o",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 24)))),
-                    TextSpan(
-                        text: "C",
-                        style: TextStyle(color: Colors.white, fontSize: 48))
-                  ]))
-            ],
-          ),
+                                style: AppTheme.WeatherDetailsTextStyle(24)),
+                          )),
+                          TextSpan(
+                              text: "C",
+                              style: AppTheme.WeatherDetailsTextStyle(48))
+                        ]))
+                  ],
+                ),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ));
   }
 }
